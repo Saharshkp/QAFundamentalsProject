32fb89c1b90b56e06fc1a45d7da05c561516d6c1
Saharshkp <104978039+Saharshkp@users.noreply.github.com>
2022-06-17 14:42:15 +0100

    Final

--- application/forms.py
+++ application/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField, PasswordField, validators
 from wtforms.validators import DataRequired, Length, ValidationError
 
+
 class directorForm(FlaskForm):
     director_name = StringField('Director: ', [validators.length(min=1, max=25), validators.DataRequired()])
     submit = SubmitField('Add')
